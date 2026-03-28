6e54bb7ec8f28140efe6e29127049d59ef020e59
Archila97 <jeniferarchila21@gmail.com>
2026-03-27 22:48:46 -0600

    commit personaje

--- src/practica2/PRACTICA2.java
+++ src/practica2/PRACTICA2.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package practica2;
 
 /**
  *
- * @author Usuario
+ * @202207231 Jenifer Carolina Archila Gómez
  */
+import vistas.VentanaMenu;
+
 public class PRACTICA2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 1objeto para el menu
+        PantallaPrincipal inicio = new PantallaPrincipal();
+
+        // mostramos al usuario
+        inicio.setVisible(true);
     }
-    
-}
---
+++ src/practica2/datos/Personaje.java
@@ -0,0 +1,27 @@
+
+package datos;
+
+public class Personaje {
+    private String carnet;
+    private String apodo;
+    private String modeloEscoba; 
+    private int retraso; // El tiempo de espera en s
+    private int puntos;
+
+    public Personaje(String carnet, String apodo, String modelo, int retraso) {
+        this.carnet = carnet;
+        this.apodo = apodo;
+        this.modeloEscoba = modelo;
+        this.retraso = retraso;
+        this.puntos = 0;
+    }
+
+    // leer tabla
+    public String getCarnet() { return carnet; }
+    public String getApodo() { return apodo; }
+    public String getModeloEscoba() { return modeloEscoba; }
+    public int getRetraso() { return retraso; }
+    public int getPuntos() { return puntos; }
+    
+    public void setPuntos(int puntos) { this.puntos = puntos; }
+}
